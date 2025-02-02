857f3ddb1b92b60afbdae3b749fd5328f1ad75cf
Hiroshi Tsubokawa <tsubo164@gmail.com>
2025-02-02 11:00:27 -0800

    Update turbine.rb

--- docs/_plugins/turbine.rb
+++ docs/_plugins/turbine.rb
@@ -1,15 +1,20 @@
-require 'rouge'
+#require 'rouge'
 
-class Rouge::Lexers::Turbine < Rouge::RegexLexer
-  title "Turbine"
-  desc "Turbine programming language"
-  tag "turbine"
-
-  state :root do
-    rule /\b(if|else|return|print)\b/, Keyword
-    rule /"(?:\\.|[^"\\])*"/, Str
-    rule /\b\d+\b/, Num
-    rule /\/\/[^\n]*/, Comment
-    rule /\w+/, Name::Variable
+#class Rouge::Lexers::Turbine < Rouge::RegexLexer
+module Rouge
+  module Lexers
+    class Python < RegexLexer
+      title "Turbine"
+      desc "Turbine programming language"
+      tag "turbine"
+    
+      state :root do
+        rule /\b(if|else|return|print)\b/, Keyword
+        rule /"(?:\\.|[^"\\])*"/, Str
+        rule /\b\d+\b/, Num
+        rule /\/\/[^\n]*/, Comment
+        rule /\w+/, Name::Variable
+      end
+    end
   end
 end
