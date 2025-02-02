f3a77215d87038013ebde7f51c05a5f779009f37
Hiroshi Tsubokawa <tsubo164@gmail.com>
2025-02-02 10:54:41 -0800

    Update turbine.rb

--- docs/_plugins/turbine.rb
+++ docs/_plugins/turbine.rb
@@ -1,15 +1,15 @@
 require 'rouge'
 
-class Rouge::Lexers::MyLang < Rouge::Lexer
+class Rouge::Lexers::Turbine < Rouge::Lexer
   title "Turbine"
   desc "Turbine programming language"
   tag "turbine"
 
   state :root do
     rule /\b(if|else|return|print)\b/, Keyword
-    #rule /"(?:\\.|[^"\\])*"/, Str
-    #rule /\b\d+\b/, Num
-    #rule /\/\/[^\n]*/, Comment
-    #rule /\w+/, Name::Variable
+    rule /"(?:\\.|[^"\\])*"/, Str
+    rule /\b\d+\b/, Num
+    rule /\/\/[^\n]*/, Comment
+    rule /\w+/, Name::Variable
   end
 end
